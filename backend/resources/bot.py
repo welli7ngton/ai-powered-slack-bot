9d0cce17cdcd0d7485e686640b5def20098b27c6
welli7ngton <welli7ngton@zorin>
2025-01-23 23:07:46 -0300

    feat: finish api and resources

--- backend/resources/bot.py
+++ backend/resources/bot.py
@@ -1,3 +1,6 @@
+import redis
+
+from time import sleep
 from slack_sdk.errors import SlackApiError
 from flask_restful import Resource
 from flask import jsonify, make_response, request
@@ -6,6 +9,7 @@ from services.bot_service import BotService
 from services.llm_service import LLMService
 from utils.verify_slack_request import verify_slack_request
 
+REDIS_CLIENT = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 class BotAnswerMessage(Resource):
     __service__ = BotService()
@@ -19,20 +23,31 @@ class BotAnswerMessage(Resource):
 
         # URL Verification Challenge
         if data.get("type") == "url_verification":
+            
             return jsonify({"challenge": data.get("challenge")})
 
         if data.get("type") == "event_callback":
+            event_id = data.get("event_id")
+            if REDIS_CLIENT.get(event_id):
+                sleep(2)
+                return make_response("", 200)
+            
+            REDIS_CLIENT.set(event_id, "processed", ex=300)
+            
             event = data.get("event", {})
             if event.get("type") == "app_mention":
                 text = event.get("text")
                 channel = event.get("channel")
-                ts = event.get("ts")
-
-                response_text = self.__llm_service__.ask_question(text)
 
                 try:
+                    response_text = self.__llm_service__.ask_question(text)
+                    sleep(2)
                     # TODO: Handle response
-                    self.__service__.send_message(message=response_text, channel_name=channel, thread_ts=ts)
+                    self.__service__.send_message(
+                        message=response_text, channel_name=channel
+                    )
+                    return make_response("", 200)
+
                 except SlackApiError as e:
                     print(f"Error sending message: {e.response['error']}")
         return make_response("", 200)
