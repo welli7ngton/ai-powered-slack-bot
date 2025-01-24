8d5c54fef54a84f0fee9ac4741fc06d176059086
Wellington Almeida <wellingtonasilva45@gmail.com>
2025-01-23 23:07:05 -0300

    feat: update services

--- backend/services/bot_service.py
+++ backend/services/bot_service.py
@@ -2,26 +2,22 @@ from os import environ
 from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from slack_sdk.web.slack_response import SlackResponse
 
 
 class BotService:
-    __username__ = 'ask-me'
-
     def __init__(self):
         load_dotenv()
-        self.client = WebClient(environ['APP_OAUTH_TOKEN'])
-        self.secret = environ['SLACK_CLIENT_SECRET']
+        self.client = WebClient(environ["APP_OAUTH_TOKEN"])
+        self.secret = environ["SLACK_SIGING_SECRET"]
 
-    def send_message(self, message: str, channel_name: str, thread_ts: str) -> SlackResponse:
+    def send_message(
+        self, message: str, channel_name: str, *, thread_ts: str | None = None
+    ):
         try:
             # TODO: Handle response
             self.client.chat_postMessage(
-                channel=channel_name,
-                text=message,
-                thread_ts=thread_ts,
-                as_user=self.__username__
+                channel=channel_name, text=message, thread_ts=thread_ts, as_user=True
             )
             return {"test": "test post request."}
         except SlackApiError as e:
-            raise Exception(e.response['error'])
+            raise Exception(e.response["error"])
--- backend/services/llm_service.py
+++ backend/services/llm_service.py
@@ -14,18 +14,19 @@ class LLMService:
             base_url="https://api-inference.huggingface.co/models/Qwen/Qwen2.5-72B-Instruct/v1",
         )
 
-    def ask_question(self, text: str, *, max_chars: int = 500) -> str:
+    def ask_question(self, text: str, *, max_chars: int = 200) -> str:
         prompt_template = PromptTemplate(
-            input_variables=["text", "max_chars"],
+            input_variables=["text"],
             template="""
-                You are an expert in the Brazilian accounting system, legislation and a technology specialist,
-                a personal assistant for a company called accord accounting. That said, you should use your
-                knowledge to answer the following question: {text} Answer me in portuguese and with {max_chars}
-                amount chars.
-            """,
+            You are an expert in the Brazilian accounting system, legislation and a technology specialist, a
+            personal assistant for a company called Accord Contabilidade. That said, you should use your knowledge
+            to answer the following question: {text} Answer me in portuguese.
+            """.replace(
+                "@", ""
+            ),
         )
 
-        prompt = prompt_template.format(text=text, max_chars=max_chars)
+        prompt = prompt_template.format(text=text)
 
         response = self.llm.invoke(prompt)
 
