f84f5396d3f2c60c0a9e22dc77d16b576f9a7c49
Wellington Almeida <wellingtonasilva45@gmail.com>
2025-02-13 09:18:45 -0300

    feat: update app.py and add wsgi configs

--- backend/app.py
+++ backend/app.py
@@ -22,14 +22,3 @@ def create_app():
     api.add_resource(BotAnswerMessage, "/slack/events")
 
     return app
-
-
-if __name__ == "__main__":
-    cert_path = os.getenv("CERT_PATH")
-    app = create_app()
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True,
-        ssl_context=(f"{cert_path}cert.pem", f"{cert_path}key.pem"),
-    )
